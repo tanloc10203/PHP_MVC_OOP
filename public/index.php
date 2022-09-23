dec651af98b94a7aa07f5336b34825eed6ce25ff
Phan_Tan_Loc <ginga550504@gmail.com>
2022-09-23 20:48:16 +0700

    update

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use app\controllers\AuthController;
 use app\controllers\SiteController;
 use app\core\Application;
+use app\models\User;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -10,6 +11,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 $config = [
+  'userClass' => User::class,
   'db' => [
     'dsn' => $_ENV['DB_DSN'],
     'user' => $_ENV['DB_USER'],
@@ -20,12 +22,18 @@ $config = [
 $app = new Application(dirname(__DIR__), $config);
 
 $app->router->get('/', [SiteController::class, 'home']);
+
 $app->router->get('/contact', [SiteController::class, 'contact']);
-$app->router->post('/contact', [SiteController::class, 'handleContact']);
+$app->router->post('/contact', [SiteController::class, 'contact']);
 
 $app->router->get('/login', [AuthController::class, 'login']);
 $app->router->post('/login', [AuthController::class, 'login']);
+
 $app->router->get('/register', [AuthController::class, 'register']);
 $app->router->post('/register', [AuthController::class, 'register']);
+
+$app->router->get('/logout', [AuthController::class, 'logout']);
+
+$app->router->get('/profile', [AuthController::class, 'profile']);
 
 $app->run();
